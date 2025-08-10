89a467ea2f2db2bce40c0ce388d7db5e05ab8a42
Your Name <you@example.com>
2025-08-10 21:17:14 +0600

    remove unexpacted axios

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      external: ["axios"],
-    },
-  },
 });
